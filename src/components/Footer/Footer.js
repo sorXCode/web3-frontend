834cf83246da72d43cf64698f423e9a7857fa9da
sorxcode <sorxcode@gmail.com>
2022-03-27 11:48:00 +0100

    updated footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <footer className={Styles.root}>
         <p className={Styles.text}>Copyright &copy; Web3Bridge {new Date().getFullYear()}</p>
+        <p className={Styles.text}>@sorxcode</p>
     </footer>
   )
 }
